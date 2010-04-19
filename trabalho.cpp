c2d2202fa4a7c8fb233fd01883c42ccb7b6959b5
Filipe Marques Pereira <lp.mp90@gmail.com>
2010-04-18 22:20:34 -0300

    o ep deve ser implementado aqui

---
+++ trabalho.cpp
@@ -0,0 +1,38 @@
+#include "palavras.h"
+
+// crie neste arquivo as funcoes solicitadas e todas as demais rotinas necessarias para sua execucao
+// tendo em mente que apenas o que esta implementado neste arquivo sera testado para fins de avaliacao
+// do seu trabalho. Nao modifique as assinaturas destas funcoes, nem o conteudo de palavras.h, ou seu
+// programa nao passara nos testes.
+
+char *aluno1() {
+	return ("XXXXXXXXXXXX");  // escreva aqui o seu nome
+}
+
+char *aluno2() {
+	return ("XXXXXXXXXXXX");  // escreva aqui o seu nome
+}
+
+char *nrousp1() {
+	return ("XXXXXXX"); // escreva aqui o seu nro USP
+}
+
+char *nrousp2() {
+	return ("XXXXXXX"); // escreva aqui o seu nro USP
+}
+
+char *turma() {
+	return ("xx"); // escreva aqui o nro da sua turma com 2 digitos
+}
+
+// somente para turma 04
+void excluirPalavra(NO *frase, NO *palavra)
+{
+     // implemente aqui a funcao solicitada ou deixe como esta (sem apagar)
+}
+
+// somente para turma 94
+NO *codificar (NO *frase)
+{
+     // implemente aqui a funcao solicitada ou deixe como esta (sem apagar)
+}
